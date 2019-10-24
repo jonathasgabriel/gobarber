63919f8c27ad9ec9d0255dea572d1f0bb2e24ad2
jonathas harb <jonathasgabriel05@gmail.com>
2019-10-24 11:46:48 -0300

    initial commit

---
+++ backend/src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+  secret: '068c7beb003f0e5e3c81745e9fe33324',
+  expiresIn: '7d',
+};
---
+++ backend/src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: '2525',
+  secure: false,
+  auth: {
+    user: '371d49232fc73a',
+    pass: 'add364064b8759',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
